549ae2a1581efca4e5fec6c6aab188263d715ee5
向蓉 <xiangrong0514@163.com>
2024-07-18 17:19:08 +0800

    修改banner

--- services/index.js
+++ services/index.js
@@ -1,16 +1,18 @@
 
-	export const request = (url, options = {}) => {
-		return new Promise((resolve, reject) => {
-			uni.request({
-				url,
-				...options,
-				success: resolve,
-				fail: reject
-			})
-		})  
-	}
+export const request = (method, url,
+	options = {}) => {
+	return new Promise((resolve, reject) => {
+		uni.request({
+			method,
+			url,
+			...options,
+			success: resolve,
+			fail: reject
+		})
+	})
+}
 	
 	export const getBannerApi = () => {
-		return request('http://121.89.213.194:5001/banner')
+		return request('post', 'http://121.89.213.194:5001/banner')
 	}
 
