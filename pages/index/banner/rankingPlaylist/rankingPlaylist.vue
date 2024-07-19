578ab3a8c1b2b97a43a67d89f83d78db5fac4588
向蓉 <xiangrong0514@163.com>
2024-07-19 21:41:39 +0800

    拆分歌单组件

--- pages/index/banner/rankingPlaylist/rankingPlaylist.vue
+++ pages/index/banner/rankingPlaylist/rankingPlaylist.vue
@@ -10,6 +10,7 @@
 		onLoad
 	} from '@dcloudio/uni-app'
 	import commentVue from './comment/comment.vue'
+	import songsLIstVue from './songsLIst/songsLIst.vue'
 
 	const id = ref('')
 	const playList = ref(null)
@@ -63,35 +64,7 @@
 			</view>
 		</view>
 
-		<view class="list">
-			<view class="list-tit">
-				<view class="icon">
-					<svg t="1721374876019" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
-						p-id="4478" id="mx_n_1721374876020" width="28" height="28">
-						<path
-							d="M512 0C230.4 0 0 230.4 0 512s230.4 512 512 512 512-230.4 512-512S793.6 0 512 0z m0 981.333333C253.866667 981.333333 42.666667 770.133333 42.666667 512S253.866667 42.666667 512 42.666667s469.333333 211.2 469.333333 469.333333-211.2 469.333333-469.333333 469.333333z"
-							fill="#dc0019" p-id="4479"></path>
-						<path
-							d="M672 441.6l-170.666667-113.066667c-57.6-38.4-106.666667-12.8-106.666666 57.6v256c0 70.4 46.933333 96 106.666666 57.6l170.666667-113.066666c57.6-42.666667 57.6-106.666667 0-145.066667z"
-							fill="#dc0019" p-id="4480"></path>
-					</svg>
-				</view>
-				<text>播放全部({{playList.tracks.length}})</text>
-			</view>
-			<view class="songs-list">
-				<view class="song-item" v-for="(item, index) in playList.tracks" :key="item.id">
-					<text>{{index + 1}}</text>
-					<view class="song-detail">
-						<view class="song-name">
-							<text>{{item.name}}</text>
-							<text>{{item.ar[0].name}}</text>
-						</view>
-						<uni-icons type="forward" size="16" color="grey"></uni-icons>
-					</view>
-				</view>
-			</view>
-		</view>
-
+		<songsLIstVue :list="playList.tracks"></songsLIstVue>
 		<commentVue v-if="show" :id="playList.id" @hide="show = false"></commentVue>
 	</view>
 </template>
