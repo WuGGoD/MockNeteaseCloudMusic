860192ba19d36c3381c500ae0f39b5d9d6a898c9
cavanagh <bartholomewFlash@qq.com>
2024-07-19 08:54:01 +0800

    songList

--- pages/section/radar.vue
+++ pages/section/radar.vue
@@ -1,8 +1,21 @@
-<template>
-</template>
-
-<script>
+<script setup>
+import { ref } from 'vue';
+import songList from './list.vue';
+import { getListApi } from '../../services/index.js';
+const list = ref(null);
+const getList = async () => {
+    const res = await getListApi();
+    console.log(res.result);
+    list.value = res.result;
+};
+getList();
 </script>
 
-<style>
-</style>
+<template>
+    <songList
+        v-for="(item, index) in 5"
+        :key="item"
+        :songList="list?.slice(index * 6 + 1, (index + 1) * 6)" />
+</template>
+
+<style></style>
